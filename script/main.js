31263c0582b5a5eefc2716e79bc555995c2b2e5e
Dmytro Gamza <gamzadmytro@gmail.com>
2020-07-28 18:53:55 +0300

    add note

--- script/main.js
+++ script/main.js
@@ -8,3 +8,10 @@ myVeriable = "Bob";
 */
 
 // this is comment
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    alert('Galych - old capital of Medeval ages!');
+} else {
+    alert('Awwww, but chocolate is my favorite...');
+}
